59077a60ef2535e46eb86f291b83ac3d04dc40dc
johnmarinelli <john.marinelli.dev@gmail.com>
2016-08-11 09:47:17 -0700

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to music-mart
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject music-mart "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [ring/ring-mock "0.3.0"]
+                 [com.taoensso/carmine "2.14.0"] ; redis
+                 [compojure "1.5.1"] ; routing
+                 ])
